98b037d330f4dd6afaa43c5778f3d1c3e3d0dcb6
LordSquishers <lordsquisherton@gmail.com>
2017-10-19 21:39:48 +0100

    TexturedModel created

---
+++ src/treeEngine/models/TexturedModel.java
@@ -0,0 +1,23 @@
+package treeEngine.models;
+
+/**
+ * Class to hold a model and a texture, for easy rendering.
+ */
+public class TexturedModel {
+
+    private RawModel rawModel;
+    private ModelTexture texture;
+
+    public TexturedModel(RawModel model, ModelTexture texture) {
+        this.rawModel = model;
+        this.texture = texture;
+    }
+
+    public RawModel getRawModel() {
+        return rawModel;
+    }
+
+    public ModelTexture getTexture() {
+        return texture;
+    }
+}
