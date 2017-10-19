8ba6d7392543fdc9cc44f2aa04074c3a75147563
LordSquishers <lordsquisherton@gmail.com>
2017-10-19 15:06:50 +0100

    Added base files
    Need to use different launch profiles for different machines.

---
+++ src/core/ElginMain.java
@@ -0,0 +1,11 @@
+package core;
+
+import data.Reference;
+
+public class ElginMain {
+
+    public static void main(String[] args) {
+        System.out.print(Reference.WINDOW_TITLE);
+    }
+
+}
---
+++ src/data/Reference.java
@@ -0,0 +1,8 @@
+package data;
+
+public class Reference {
+
+    public static final String VERSION = "0.0.1";
+    public static final String WINDOW_TITLE = "Elgin v" + VERSION;
+
+}
